fb6e62a3fda4f636d02da28310eccc641c0ffea9
superuser <superuser@devsys>
2023-03-15 14:54:39 +0500

    Added A Splash Screen with Animation

---
+++ lib/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:hey_taxi/constants.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: Duration(seconds: 2), vsync: this)
+        ..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override 
+  initState() { 
+	  var future = Future.delayed(
+		  Duration(seconds: 3), () => {
+			  Navigator.pushNamed(context, '/login'),
+		  }
+	  );
+  }
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Column(
+	mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ScaleTransition(
+              scale: _animation,
+              child: const Image(
+                image: AssetImage('assets/images/Tawakkal-logo.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
